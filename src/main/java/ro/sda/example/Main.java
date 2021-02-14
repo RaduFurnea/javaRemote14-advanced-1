c6f25a27a5b428cc774e5b358f07ca507e34e0ab
RaduFurnea <radufurnea@gmail.com>
2021-02-14 10:05:32 +0200

    homework solutions

--- src/main/java/ro/sda/example/Main.java
+++ src/main/java/ro/sda/example/Main.java
@@ -1,18 +1,20 @@
 package ro.sda.example;
 
+import ro.sda.example.entity.Group;
 import ro.sda.example.entity.Student;
+import ro.sda.example.entity.Trainer;
 import ro.sda.example.exception.MaximumNumberOfStudentsReached;
 import ro.sda.example.exception.SeniorityLevelInvalid;
 import ro.sda.example.repository.Repository;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 
     private static final Repository repo = Repository.getInstance();
 
     public static void main(String[] args) {
+
         System.out.println("Printing list sizes...");
         System.out.println(repo.getStudents().size() + " " + repo.getTrainers().size() + " " + repo.getGroups().size());
 
@@ -23,7 +25,104 @@ public class Main {
 
         System.out.println("------------ Printing all students assigned to groups that are under 25 years old ---------------");
         studentsUnder25();
+
+        System.out.println("------------ Display all students with previous Java knowledge -----------------");
+        studentsWithPreviousJavaKnowledge();
+
+        groupWithNoPreviousJavaKnowledge();
+
+        groupWithMaxStudents();
+
+        removeStudentsYoungerThan25();
+
+        displayStudentsByTrainer();
     }
+
+    //1
+    private static void studentsWithPreviousJavaKnowledge() {
+        repo.getStudents()
+                .stream()
+                .filter(Student::isHasPreviousJavaKnowledge)
+                .forEach(System.out::println);
+    }
+
+    //2
+    private static void groupWithNoPreviousJavaKnowledge() {
+        Group groupWithMax = null;
+        long max = 0;
+
+        if (repo.getGroups().isEmpty()) {
+            System.out.println("There are no groups.");
+            return;
+        }
+
+        for (Group group : repo.getGroups()) {
+            long groupCount = group.getStudents().stream().filter(s -> !s.isHasPreviousJavaKnowledge()).count();
+            if (groupWithMax == null) {
+                groupWithMax = group;
+            }
+            if (groupCount > max) {
+                max = groupCount;
+                groupWithMax = group;
+            }
+        }
+        System.out.println(groupWithMax.getName() + " has " + max + " students with no previous Java knowledge");
+    }
+
+    //3
+    public static void groupWithMaxStudents() {
+        Group groupWithMaxStudents = repo.getGroups().get(0);
+
+        for (Group g : repo.getGroups()) {
+            if (g.getStudents().size() > groupWithMaxStudents.getStudents().size()) {
+                groupWithMaxStudents = g;
+            }
+        }
+        System.out.println("Group with max students: " + groupWithMaxStudents.getName() + " has " + groupWithMaxStudents.getStudents().size());
+    }
+
+    //4
+    private static void removeStudentsYoungerThan25() {
+        for (Group g : repo.getGroups()) {
+            for (Student s : repo.getStudents()) {
+                if (s.getAge() < 25) {
+                    g.removeStudent(s);
+                }
+            }
+        }
+
+        repo.getGroups().forEach(g -> {
+            System.out.println("Trainer is: " + g.getTrainer().getName() + " for group " + g.getName());
+            g.getStudents().stream().map(Student::getName).forEach(System.out::println);
+            System.out.println("---------------------------------------");
+        });
+    }
+
+    //5
+    private static void displayStudentsByTrainer() {
+        System.out.println("Students grouped by trainer: ");
+
+        Map<Trainer, Set<Student>> studentsByTrainerMap = new HashMap<>();
+
+        for (Group g : repo.getGroups()) {
+            // get the set for the current trainer (can be null)
+            Set<Student> studForTrainer = studentsByTrainerMap.get(g.getTrainer());
+            // this if - then will execute every time we find a group that doesn't have their trainer in the map
+            if (studForTrainer == null) {
+                studForTrainer = new HashSet<>(g.getStudents());
+                studentsByTrainerMap.put(g.getTrainer(), studForTrainer);
+            } else {
+                studForTrainer.addAll(new HashSet<>(g.getStudents()));
+            }
+        }
+
+        for (Map.Entry<Trainer, Set<Student>> entry : studentsByTrainerMap.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue().size() + " students");
+            System.out.println(entry.getValue() + "\n");
+        }
+    }
+
 
     private static void putPeopleInGroups() {
         try {
